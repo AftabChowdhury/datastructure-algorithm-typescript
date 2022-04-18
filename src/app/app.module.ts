73539baab6f8ec2d7a941d1e687b128f8b92f23e
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-04-18 17:47:59 +0200

    RunLengthEncodingComponent: implement RunLengthEncoding solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HaspMapComponent } from './components/hasp-map/hasp-map.component';
+import { RunLengthEncodingComponent } from './components/run-length-encoding/run-length-encoding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HaspMapComponent
+    HaspMapComponent,
+    RunLengthEncodingComponent
   ],
   imports: [
     BrowserModule,
